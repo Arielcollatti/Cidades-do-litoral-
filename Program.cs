7cb57db9b29bbb595c4da81ca524091ef45b5b5d
arielcollatti <colatti2017@gmail.com>
2023-03-08 14:49:38 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Cidades-Litoral-sul");
+Console.WriteLine("");
+
+Console.WriteLine("Peruíbe");
+Console.WriteLine("Iaanhaém");
+Console.WriteLine("Ilha Compria");
+Console.WriteLine("Iguape");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Cananéia");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Registro");
+Console.WriteLine("Sete Barras");
+Console.WriteLine("Eldorado");
+Console.WriteLine("Pedro de Toledo");
+Console.WriteLine("Juquiá");
+Console.WriteLine("Pariquera-Açu");
+Console.WriteLine ("Cajati");
+Console.WriteLine("Itariri");
+Console.WriteLine("Miracatu");
+Console.WriteLine("jacupiranga");
